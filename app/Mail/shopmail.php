b8596847b260b1cdaa6434e6f4e67869856aec19
joel goyo <joelgoyosaez@gmail.com>
2021-07-16 09:39:40 -0400

    emails

---
+++ app/Mail/shopmail.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class shopmail extends Mailable
+{
+    use Queueable, SerializesModels;
+    
+    public $shopmail = "compra Realizada";
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+       
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('mail.compra');
+    }
+}
