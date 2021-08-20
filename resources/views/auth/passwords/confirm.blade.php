d45a9c0181330aa73f6dc64dc7106d254842b68d
Zelaznog-2 <60021925+Zelaznog-2@users.noreply.github.com>
2021-08-20 10:09:29 -0400

    cambiar el dise#o para recuperar la clave

--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -1,49 +1,54 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Confirm Password') }}</div>
+<!-- BEGIN: Content-->
+<div class="auth-wrapper auth-v2">
+    <div class="auth-inner row m-0">
 
-                <div class="card-body">
-                    {{ __('Please confirm your password before continuing.') }}
-
-                    <form method="POST" action="{{ route('password.confirm') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Confirm Password') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+    
+    <!-- Left bg-->
+    <div class="d-none d-lg-flex col-lg-8 align-items-center legazy_bg">
+        <div class="align-items-center justify-content-center">
+            <div class= "row justify-content-center">
+                <div class="col-auto">
+                     <img src="{{ asset('assets/img/legazy_pro/logo.svg') }}" alt="">
                 </div>
             </div>
         </div>
     </div>
+
+    <!-- Login-->
+    <div class="d-flex col-lg-4 align-items-center auth-bg p-lg-4">
+        
+        <div class="row">
+            <div class="col-12 col-sm-8 col-md-6 col-lg-12 px-xl-2">
+                <h2 class="card-title fw-bold mb-1 text-white">Confirmar Clave</h2>
+                <form class="auth-login-form mt-2" action="{{ route('password.confirm')}}" method="POST">
+                    @csrf
+                    <div class="mb-1">
+                        <label class="form-label text-white" for="username">Clave</label>
+                        <input class="form-control border border-warning rounded-0 @error('password') is-invalid @enderror" id="password" type="email" required name="password"
+                            placeholder="clave actual" aria-describedby="password" autofocus="" tabindex="1" />
+                            @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+
+                    <button class="btn btn-primary w-100 rounded-0 mt-2" type="submit" tabindex="4">Confirmar Clave</button>
+
+                    <a class="btn btn-primary w-100 rounded-0 mt-2" href="{{ route('password.request') }}">Recuperar clave</a>
+
+                </form>
+                {{-- <p class="text-center mt-2"><span>¿Nuevo en la plataforma?</span><a
+                        href="{{ route('register') }}"><span>&nbsp;<b>Crea una cuenta</b></span></a></p> --}}
+            </div>
+        </div>
+
+    </div>
+    <!-- /Login-->
 </div>
+</div>
+<!-- END: Content-->
 @endsection
