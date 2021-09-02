33daca40301b701c833711a007130c4588dd5d17
Alexis Jose Valera Valbuena <alexisjoseva95@gmail.com>
2021-09-02 14:03:04 -0400

    upd

--- resources/views/settlement/withdraw.blade.php
+++ resources/views/settlement/withdraw.blade.php
@@ -6,6 +6,42 @@
     .bg-gris{
         background: #5f5f5f5f;
     }
+    a.btn.btn-primary.d.waves-effect.waves-light {
+    margin-top: 15px;
+    padding: 12px;
+    font-size: 22px;
+}
+    h2.card-title.text-white {
+    font-size: 24px !important;
+    line-height: 34px;
+    font-weight: 600;
+}
+p.text-white5 {
+    font-size: 40px;
+    padding: 5px;
+}
+.form-control {
+    background: #141414 !important;
+}
+h5.text-white {
+    font-size: 14px;
+    line-height: 20px;
+}
+input#to::placeholder {
+    font-size: 12px;
+    background: #141414;
+    border: 0px;
+}
+p.text-white1 {
+    margin-left: 5px;
+    font-size: 14px !important;
+    font-weight: 600;
+}
+p.text-white1 span {
+    font-weight: 400;
+    font-size: 12px !important;
+    padding-left: 5px;
+}
 </style>
 @endpush
 
@@ -16,54 +52,50 @@
 
 @section('content')
 <div>
-    <div class="col-12">
+    <div class=" col-8 offset-md-2">
         <div class="card bg-lp">
             <div class="card-header">
-                <h4 class="card-title text-white">Retiros</h4>
+                <h2 class="card-title text-white">Retirar Ganancias</h2>
             </div>
             <div class="card-content">
                 <div class="card-body card-dashboard">
                     <form action="">
                         <div class="row">
                             <div class="col-12 mb-1">
-                                <h5 class="text-white">Moneda: <b> USDT THETER</b> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Tether_Logo.svg/1280px-Tether_Logo.svg.png" alt="" height="35"></h5>
+                                <h5 class="text-white">Moneda</h5>
+                                <p class="text-white1"> <img src="https://icons.iconarchive.com/icons/cjdowner/cryptocurrency-flat/1024/Tether-USDT-icon.png" alt="" height="24"> USDT <span >TetherUS</span></p>
                             </div>
-                            <div class="col-12 col-md-7 mb-1">
-                                <label for="">Billetera</label>
-                                <input type="text" name="wallet" class="form-control bg-gris">
+                            <div class="col-12 col-md-12 mb-1">
+                                <h5 class="text-white">Dirección</h5>
+                                <input type="text" id="to" placeholder="Introduce aquí la dirección" name="wallet" class="form-control bg-gris">
                             </div>
-                            <div class="col-12 col-md-5 mb-1">
-                               <div class="row">
-                                <div class="col-12 col-md-6">
-                                    <h5 class="text-white">
-                                        Disponible:
-                                        <b>{{Auth::user()->saldoDisponible()}}</b>
-                                    </h5>
-                                </div>
-                                <div class="col-12 col-md-6">
-                                    <h5 class="text-white">
-                                        Retiro Minimo:
-                                        <b> 50$ </b>
-                                    </h5>
-                                </div>
-                                <div class="col-12 col-md-6">
-                                    <h5 class="text-white">
-                                        Fee: <b>6%</b>
-                                        <br>
-                                        Total Fee:
-                                        <b>{{ number_format(Auth::user()->getFeeWithdraw(), 2) }}</b>
-                                    </h5>
-                                </div>
-                                <div class="col-12 col-md-6">
-                                    <h5 class="text-white">
-                                        A Recibir:
-                                        <b>{{ number_format(Auth::user()->totalARetirar(),2) }}</b>
-                                    </h5>
-                                </div>
-                               </div>
+                            <div class="col-12 col-md-12 mb-1">
+                                <h5 class="text-white">Red</h5>
+                                <p class="text-white1"> TRX <span >Tron (TRC20)</span></p>
                             </div>
-                            <div class="col-12 text-center">
-                                <a href="" class="btn btn-primary">Retirar</a>
+                            <div class="col-6 col-md-6 mb-1">
+                                <h5 class="text-white">Saldo en USDT</h5>
+                                <p class="text-white1"> {{Auth::user()->saldoDisponible()}}</p>
+                            </div>
+                            <div class="col-6 col-md-6 mb-1">
+                                <h5 class="text-white">Retiro Minimo</h5>
+                                <p class="text-white1"> 50 USDT</p>
+                            </div>
+                            <div class="col-6 col-md-6 mb-1">
+                                <h5 class="text-white">% de Fee de Retiro</h5>
+                                <p class="text-white1"> 6%</p>
+                            </div>
+                             <div class="col-6 col-md-6 mb-1">
+                                <h5 class="text-white">Monto comision</h5>
+                                <p class="text-white1">{{ number_format(Auth::user()->getFeeWithdraw(), 2) }}</p>
+                            </div>
+                            <div class="col-6 col-md-6 mb-1">
+                                <h5 class="text-white">Importe que se recibirá</h5>
+                                <p class="text-white5">{{ number_format(Auth::user()->totalARetirar(),2) }} USDT</p>
+                               
+                            </div>
+                             <div class="col-6 col-md-6 mb-1">
+                                <a href="" class="btn btn-block btn-primary d">Retirar</a>
                             </div>
                         </div>
                     </form>
